4a0a0c5096c7ede5d3835aa967280221abd8c4dd
Priyush02K <collegeinfopk02@gmail.com>
2025-12-01 13:13:42 +0530

    Add Priyush.java for PR

---
+++ src/com/anudip/project/PriyushTest.java
@@ -0,0 +1,11 @@
+package com.anudip.project;
+
+public class PriyushTest {
+
+    public static void main(String[] args) {
+        System.out.println("Java test file created successfully!");
+        System.out.println("This file was pushed to a new branch.");
+    }
+}
+
+
